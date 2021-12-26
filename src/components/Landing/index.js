c79851da5bca003f4af0490eb2e45f98e5d98306
Morgan Huegel <huegel@3cisd.com>
2021-12-26 01:18:46 -0500

    confetti on landing page

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -1,26 +1,41 @@
 import React from "react";
+import useWindowSize from "react-use/lib/useWindowSize";
+import Confetti from "react-confetti";
 import "./style.css";
 import birthdayImg from "../../images/birthdayLogo.png";
 import raccoonImg from "../../images/raccoon.png";
 import possumImg from "../../images/possum.png";
 
-const Landing = () => (
-  <div className="landing">
-    <img src={raccoonImg} alt="Jermey the raccoon" className="jeremy" />
-    <img src={possumImg} alt="Jermiah the possum" className="jeremiah" />
-    <div className="rope-bg" />
-    <img src={birthdayImg} alt="happy birthday" className="birthday-img" />
-    <p className="critter-font">
-      mPmpMpMppMp hi KaRmIn mpmP huZbaDn and mE mAde u somE riDDles mpmPmp haPPy
-      biRthdAy
-    </p>
-    <p className="critter-font signature">
-      jeRemy thE Racc0on{" "}
-      <span role="img" ariaLabel="Paw Print">
-        {String.fromCodePoint("0x1F43E")}
-      </span>
-    </p>
-  </div>
-);
+const Landing = () => {
+  const { width, height } = useWindowSize();
+  console.log("width", width);
+
+  return (
+    <div className="landing">
+      <Confetti
+        width={width}
+        height={height}
+        numberOfPieces={width < 500 ? 100 : 200}
+      />
+      <img src={raccoonImg} alt="Jermey the raccoon" className="jeremy" />
+      <img src={possumImg} alt="Jermiah the possum" className="jeremiah" />
+      <div className="rope-bg" />
+      <img src={birthdayImg} alt="happy birthday" className="birthday-img" />
+
+      <div className="message-container">
+        <p className="critter-font">
+          mPmpMpMppMp hi KaRmIn mpmP huZbaDn and mE mAde u somE riDDles mpmPmp
+          haPPy biRthdAy
+        </p>
+        <p className="critter-font signature">
+          jeRemy thE Racc0on{" "}
+          <span role="img" aria-label="Paw Print">
+            {String.fromCodePoint("0x1F43E")}
+          </span>
+        </p>
+      </div>
+    </div>
+  );
+};
 
 export default Landing;
